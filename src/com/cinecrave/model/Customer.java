f64324d07381fceea28a5221833e055a72d43d6e
Aswathy Santhosh <aswathysanthosh7025@gmail.com>
2025-10-23 13:08:41 +0530

    Fix formatting and add newline at end of Customer.java

--- src/com/cinecrave/model/Customer.java
+++ src/com/cinecrave/model/Customer.java
@@ -1,20 +1,23 @@
 package com.cinecrave.model;
+
 import java.util.List;
 import java.util.ArrayList; 
+
+
 public class Customer extends User {
+
     private List<String> bookings; 
 
     public Customer(int userId, String name, String email, String phone) {
         super(userId, name, email, phone);
-        this.bookings = new ArrayList<>(); 
+        this.bookings = new ArrayList<>();
     }
 
     public void addBooking(String bookingId) { 
         this.bookings.add(bookingId);
     }
-    
+   
     public List<String> getBookings() {
         return this.bookings;
     }
-   
 }
